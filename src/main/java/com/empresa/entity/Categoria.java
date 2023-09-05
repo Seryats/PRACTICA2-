a86e3afc86bf083f240e1a58a29a9182a7ec5ea7
Seryats <estaseryats@gmail.com>
2023-09-05 09:25:29 -0500

    CLIENTE

---
+++ src/main/java/com/empresa/entity/Categoria.java
@@ -0,0 +1,32 @@
+package com.empresa.entity;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCategoria;
+	
+	private String nombre;
+
+}
+
+
+
